bde29913d486d6f421469f6353b81dbca6bb4b6f
victorlundqvist <103148890+cvmlundqvist@users.noreply.github.com>
2025-02-22 23:24:05 +0100

    be ändrad till cloud

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://lease-be-45ce8c8a3508.herokuapp.com/api',
 });
 
 export default api;
